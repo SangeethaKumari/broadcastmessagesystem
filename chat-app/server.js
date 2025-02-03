82ca1714c6fe189d54b6e17e3611704e68346046
SangeethaKumari <sangeethakumari.g@gmail.com>
2025-02-03 14:17:45 -0800

    Add direct messaging feature for private chats

--- chat-app/server.js
+++ chat-app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const { spawn } = require('child_process'); // For running LLAMA model locally
+const { spawn } = require('child_process');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,15 +9,13 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-// Function to generate random usernames
+let users = {};  // To store connected users
+
 function generateUsername() {
     return 'User' + Math.floor(Math.random() * 1000);
 }
 
-// Function to get response from LLAMA model
 function getBotResponse(message, callback) {
-    //const llama = spawn('./llama_model', ['--prompt', message]);
-    //const llama = spawn('../llama.cpp/build/bin/llama-cli', ['--prompt', message]);
     const llama = spawn('../llama.cpp/build/bin/llama-cli', ['-m', '../llama.cpp/models/ggml-model-f16.gguf', '--prompt', message]);
 
     let response = '';
@@ -34,25 +32,34 @@ function getBotResponse(message, callback) {
     });
 }
 
+// Handle messages
+function handleMessage(sender, message, recipient = null) {
+    if (recipient === "@bot") {
+        getBotResponse(message, (botReply) => {
+            io.to(users[sender]).emit('chat message', { user: 'LLAMA Bot', text: botReply });
+        });
+    } else if (recipient && users[recipient]) {
+        io.to(users[recipient]).emit('chat message', { user: sender, text: message });
+    } else {
+        io.emit('chat message', { user: sender, text: message });
+    }
+}
+
 io.on('connection', (socket) => {
     const username = generateUsername();
+    users[username] = socket.id;
+
     console.log(`${username} connected:`, socket.id);
-
-    // Send the assigned username to the client
     socket.emit('assign username', username);
+    io.emit('update users', Object.keys(users));  // Send updated user list
 
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', { user: username, text: msg }); // Send to all clients except sender
-
-        // Trigger LLAMA bot response if message starts with @bot
-        if (msg.toLowerCase().startsWith('@bot')) {
-            getBotResponse(msg.replace('@bot', '').trim(), (botReply) => {
-                io.emit('chat message', { user: 'LLAMA Bot', text: botReply });
-            });
-        }
+    socket.on('chat message', ({ message, recipient }) => {
+        handleMessage(username, message, recipient);
     });
 
     socket.on('disconnect', () => {
+        delete users[username];
+        io.emit('update users', Object.keys(users));  // Update user list on disconnect
         console.log(`${username} disconnected:`, socket.id);
     });
 });
